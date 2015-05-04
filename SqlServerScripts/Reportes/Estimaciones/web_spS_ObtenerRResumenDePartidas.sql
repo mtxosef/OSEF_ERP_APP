1c51dcc943348fd339f5b99c6686e3b76a72d19a
ChristianMMJ <christian.m.m91@gmail.com>
2015-05-04 15:23:39 -0500

    Reporte presupuesto

--- SqlServerScripts/Reportes/Estimaciones/web_spS_ObtenerRResumenDePartidas.sql
+++ SqlServerScripts/Reportes/Estimaciones/web_spS_ObtenerRResumenDePartidas.sql
@@ -39,7 +39,7 @@ BEGIN
    SELECT 
 		--ID DE MOVIMIENTO Y CONCEPTOS
 		OE.ID, OE.Observaciones OBRA,
-		OED.ConceptoID,OED.Cantidad, OED.Precio,OED.Importe,OED.IntExt,
+		OED.ConceptoID,OED.Cantidad, OED.Precio,OED.Importe,OED.IntExt,OE.FechaEmision,
 		--Datos de la sucursal
 		S.CR,S.Nombre Sucursal,S.Calle,S.NoExterior,S.NoInterior,C.Descripcion Colonia,M.Descripcion Municipio,E.Descripcion Estado,
 		--Datos del concepto
